f45e9a312721931b4ac37aea5433799a448edae7
Syed Mohammed Muzaffar <95693074+SMMuzaffar21@users.noreply.github.com>
2024-10-28 22:27:54 +0530

    Creating skeleton from spring initializer

---
+++ app/src/test/java/com/syed/project/ridebooking/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syed.project.ridebooking.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
